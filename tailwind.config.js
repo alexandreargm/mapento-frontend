a9709198f29eb5cb60047e484097dd56f1b2bf40
alexandreargm <alexandreargm@gmail.com>
2021-01-21 18:28:19 +0100

    Added Montserrat typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,21 @@ module.exports = {
       '2xs': '23.4375rem', // 375px
       xs: '30rem', // 480px
     },
-    backgroundColor: theme => ({
-      'light': '#ffffff',
-      'dark': '#E9EAEC',
-      'medium': '#DCDCDC'
-    }),
+    colors: {
+      'b-light': '#ffffff',
+      'b-dark': '#E9EAEC',
+      'b-medium': '#DCDCDC',
+      't-light': '#333333',
+      't-light-secondary': '#7e7e7e',
+      't-dark': '#333333',
+      't-dark-secondary': '#777D88',
+
+    },
+    fontFamily: {
+      'sans': ['Montserrat', 'system-ui'],
+      //'serif': ['ui-serif', 'Georgia'],
+      //'mono': ['ui-monospace', 'SFMono-Regular'],
+    },
     extend: {
       minWidth: {
         none: 'none',
@@ -32,14 +42,10 @@ module.exports = {
         '6xl': '72rem',
       },
       fontSize: {
-        '2xs': '0.5625rem' // 9px
+        '4xs': '0.5625rem', // 9px
+        '3xs': '0.625rem', //10px
+        '2xs': '0.6875rem', // 11px
       },
-      textColor: {
-        'light': '#333333',
-        'light-secondary': '#7e7e7e',
-        'dark': '#333333',
-        'dark-secondary': '#777D88',
-      }
     },
   },
   variants: {},
