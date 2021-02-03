8f7ab499d314b6c648ee0cbddcc65c53b49b6f52
alexandreargm <alexandreargm@gmail.com>
2021-02-03 19:08:23 +0100

    Test an apollo query

--- components/ProximosEventos.vue
+++ components/ProximosEventos.vue
@@ -1,36 +1,22 @@
 <template>
-  <div class="proximos-eventos">
-    <ApolloQuery
-      :query="gql => gql`
-        query {
-          events {
-            data {
-              id
-              title
-            }
-          }
-        }
-      `"
-    >
-      <!-- The result will automatically updated -->
-      <template slot-scope="{ result: { data, loading } }">
-        <!-- Some content -->
-        <div v-if="loading">
-          Loading...
-        </div>
-        <ul v-else>
-          <li v-for="event of data.events.data" :key="event.id">
-            {{ event.title }}
-          </li>
-        </ul>
-      </template>
-    </ApolloQuery>
-  </div>
+  <div class="proximos-eventos" />
 </template>
 
 <script>
-export default {
+import gql from 'graphql-tag'
 
+export default {
+  apollo: {
+    events: gql`
+      query {
+        events
+        {
+          id
+          title
+        }
+      }
+    `
+  }
 }
 </script>
 
