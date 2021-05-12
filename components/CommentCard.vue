5dbb3b831a2ba17bc1207e258120fc1cc3bede0e
alexandreargm <alexandreargm@gmail.com>
2021-05-12 18:01:31 +0200

    Changed card layout

---
+++ components/CommentCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <article class="comment-card bg-white rounded-md p-4 flex space-x-4">
+    <img v-if="avatar" :src="avatar" class="comment-card__user-avatar w-8 h-8 flex-shrink-0 bg-brand rounded-full">
+    <div class="comment-card__body space-y-1">
+      <div class="comment-card__comment-title font-semibold text-xs">
+        {{ title }}
+      </div>
+      <div class="comment-card__publication text-t-dark-secondary text-xs">
+        <span class="comment-card__author">{{ author }}</span>
+        ·
+        <span class="comment-card__date">{{ date }}</span>
+      </div>
+      <div v-if="commentDesctiptionComputed !== ''" class="comment-card__description text-sm">
+        {{ commentDesctiptionComputed }}
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    commentDesctiptionComputed () {
+      return this.type === 'Comment' ? 'Replied to a comment' : 'Commented'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- components/EventUpdateCard.vue
+++ components/EventUpdateCard.vue
@@ -1,21 +1,17 @@
 <template>
-  <article class="event-update-card bg-white rounded-md p-4 space-y-2">
-    <div class="event-update-card__head leading-snug text-sm">
-      <span class="event-update-card__event-title font-semibold">
+  <article class="event-update-card bg-white rounded-md p-4 flex space-x-4">
+    <img v-if="avatar" :src="avatar" class="event-card__user-avatar w-8 h-8 flex-shrink-0 bg-brand rounded-full">
+    <div class="event-update-card__body space-y-1">
+      <div class="event-update-card__event-title font-semibold text-xs leading-tight">
         {{ title }}
-      </span>
-    </div>
-    <div class="event-update-card__body flex space-x-4">
-      <img v-if="avatar" :src="avatar" class="event-card__user-avatar w-8 h-8 flex-shrink-0 bg-brand rounded-full">
-      <div class="event-update-card__details space-y-1">
-        <div class="event-update-card__publication text-t-dark-secondary text-xs">
-          <span class="event-update-card__author">{{ author }}</span>
-          ·
-          <span class="event-update-card__date">{{ date }}</span>
-        </div>
-        <div class="event-update-card__changes text-xs">
-          {{ computedTypeChanges }}
-        </div>
+      </div>
+      <div class="event-update-card__publication text-t-dark-secondary text-xs">
+        <span class="event-update-card__author">{{ author }}</span>
+        ·
+        <span class="event-update-card__date">{{ date }}</span>
+      </div>
+      <div class="event-update-card__changes text-sm">
+        {{ computedTypeChanges }}
       </div>
     </div>
   </article>
