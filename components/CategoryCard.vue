699f84426700fbe5eafc22d12bc142c2c8a29a40
alexandreargm <alexandreargm@gmail.com>
2021-06-04 10:05:22 +0200

    Style. Made category links more self evident

--- components/CategoryCard.vue
+++ components/CategoryCard.vue
@@ -1,6 +1,6 @@
 <template>
-  <article class="category-card p-4 h-16 flex items-center bg-b-light rounded-md">
-    <div class="category-card__name font-semibold leading-5">
+  <article class="category-card p-4 h-16 flex items-center cursor-pointer bg-b-light rounded-md">
+    <div class="category-card__name font-semibold leading-5 text-brand">
       {{ name }}
     </div>
   </article>
