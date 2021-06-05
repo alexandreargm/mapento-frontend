1d3d441ccabb2d9279988e42190b16a82495f5ed
alexandreargm <alexandreargm@gmail.com>
2021-06-05 18:12:52 +0200

    Stylelint. Added proper support for scss features and css properties order

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
   extends: [
-    'stylelint-config-recommended'
+    "stylelint-config-recommended",
+    "stylelint-config-recommended-scss"
+  ],
+  plugins: [
+    "stylelint-order",
+    "stylelint-scss"
   ],
   rules: {
-    'at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: [
-          'tailwind',
-          'apply',
-          'variants',
-          'responsive',
-          'layer',
-          'screen'
-        ]
-      }
-    ],
-    'declaration-block-trailing-semicolon': null,
-    'no-descending-specificity': null,
-    "no-empty-source": null
+    // Tailwindcss rules
+    "at-rule-no-unknown": null,
+    "declaration-block-trailing-semicolon": null,
+    "no-descending-specificity": null,
+    "no-empty-source": null,
+
+    // Scss rules
+    "order/properties-alphabetical-order": true,
+    "scss/at-rule-no-unknown": null,
+    "scss/at-import-no-partial-leading-underscore": null
   }
 }
