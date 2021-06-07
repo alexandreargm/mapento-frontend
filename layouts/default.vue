bd844dcfaaf5bd3c1da5e97dc865b06b6b8f9213
alexandreargm <alexandreargm@gmail.com>
2021-06-07 11:31:52 +0200

    Style. Moved layout and container rules to layout component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="layout">
-    <div class="container flex-1 sm:flex-initial">
-      <Nuxt class="page" />
+  <div class="default-layout">
+    <div class="default-layout__container flex-1 sm:flex-initial">
+      <Nuxt class="default-layout__page" />
       <AccountOverlay />
     </div>
-    <div class="layout__right-column hidden sm:flex sm:flex-1" />
-    <div class="layout__left-column sm:flex sm:flex-1 xs:order-first sm:justify-end sm:px-2">
+    <div class="default-layout__right-column hidden sm:flex sm:flex-1" />
+    <div class="default-layout__left-column sm:flex sm:flex-1 xs:order-first sm:justify-end sm:px-2">
       <MainNav />
     </div>
   </div>
@@ -17,8 +17,21 @@ export default {
 }
 </script>
 
-<style>
-.page {
-  @apply min-h-full;
-}
+<style lang="scss">
+
+  .default-layout {
+    @apply flex flex-col h-full;
+    @screen xs {
+      @apply flex-row;
+    }
+
+    &__container {
+      border: 1px solid;
+      @apply bg-b-dark relative overflow-y-auto w-full max-w-tablet-landscape border-b-medium border-t-0;
+    }
+
+    &__page {
+      @apply min-h-full;
+    }
+  }
 </style>
