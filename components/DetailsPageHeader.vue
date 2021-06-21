7d570fde8f40838069e3a2f5b947ca0c38a112c1
alexandreargm <alexandreargm@gmail.com>
2021-06-21 20:42:19 +0200

    Changed div to header

--- components/DetailsPageHeader.vue
+++ components/DetailsPageHeader.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="details-page-header flex items-center">
+  <header class="details-page-header bg-b-light flex items-center pt-4">
     <button class="flex items-center h-16 w-16 px-4" @click="routerGoBack">
       <svg-icon name="solid/arrow-left" class="svg-32" />
     </button>
-  </div>
+  </header>
 </template>
 
 <script>
