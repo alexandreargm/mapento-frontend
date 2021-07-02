13d9de76290439551d441496074da2b64ccbae1e
alexandreargm <alexandreargm@gmail.com>
2021-07-02 13:47:57 +0200

    Implemented luxon interface

--- plugins/luxon.js
+++ plugins/luxon.js
@@ -2,6 +2,28 @@ import { DateTime, Settings } from 'luxon'
 
 Settings.defaultLocale = 'en'
 
-export default ({ app }, inject) => {
-  inject('DateTime', DateTime)
+// export default ({ app }, inject) => {
+//   inject('DateTime', DateTime)
+// }
+
+function _parseFromDatabase (datetime) {
+  return DateTime.fromSQL(datetime)
+}
+
+function _setDateFormat (datetime, dateformat) {
+  const parseDate = _parseFromDatabase(datetime)
+  return parseDate.toLocaleString(dateformat)
+}
+
+export const getRelativeDate = (datetime) => {
+  const parseDate = _parseFromDatabase(datetime)
+  return parseDate.toRelative(datetime)
+}
+
+export const getLongDate = (datetime) => {
+  return _setDateFormat(datetime, DateTime.DATE_HUGE)
+}
+
+export const getMidDate = (datetime) => {
+  return _setDateFormat(datetime, DateTime.DATE_MED_WITH_WEEKDAY)
 }
