6cc8e721d7766b6292ea78805ab1f3a2d8b4092b
alexandreargm <alexandreargm@gmail.com>
2021-07-08 11:48:04 +0200

    Renamed duplicate variant  variable to modifier

--- plugins/component-utils.js
+++ plugins/component-utils.js
@@ -6,11 +6,11 @@ export const variantPropDefinition = {
 }
 
 export const getVariantString = function (name, variant) {
-  const addComponentPrefix = variant => `${name}--${variant}`
+  const addComponentPrefix = modifier => `${name}--${modifier}`
 
   if (typeof variant === 'string') {
     return addComponentPrefix(variant)
   }
 
-  return variant.map(variant => addComponentPrefix(variant))
+  return variant.map(modifier => addComponentPrefix(modifier))
 }
