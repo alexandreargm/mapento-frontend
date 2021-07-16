c5436731e532bba1d05aeba6e107e25928a95c1b
alexandreargm <alexandreargm@gmail.com>
2021-07-16 16:13:00 +0200

    Now uses Modals for filter dialogs

--- components/FeedFilter.vue
+++ components/FeedFilter.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="feed-filter flex overflow-x-auto px-4 -mb-2">
-    <slot />
+  <div class="feed-filter">
+    <div class="feed-filter__buttons flex overflow-x-auto px-4 pt-4 pb-1 space-x-2 -mb-2">
+      <slot name="buttons" />
+    </div>
+
+    <div class="feed-filter__modals">
+      <slot name="modals" />
+    </div>
   </div>
 </template>
-
-<script>
-export default {
-}
-</script>
