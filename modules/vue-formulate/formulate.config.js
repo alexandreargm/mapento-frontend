0fb6b740a717206e2d47b6cf8635f47a4d90ca35
alexandreargm <alexandreargm@gmail.com>
2021-07-16 16:10:28 +0200

    Vue-formulate styles and updated config

--- modules/vue-formulate/formulate.config.js
+++ modules/vue-formulate/formulate.config.js
@@ -2,42 +2,5 @@ import FormulateVSelectPlugin from '@cone2875/vue-formulate-select'
 
 export default {
   plugins: [FormulateVSelectPlugin],
-  locale: 'en',
-  classes: {
-    outer (context) {
-      switch (context.classification) {
-      case 'box':
-        return ''
-      default:
-        return ''
-      }
-    },
-    wrapper (context) {
-      switch (context.classification) {
-      case 'box':
-        return 'flex space-x-3'
-      default:
-        return ''
-      }
-    },
-    label (context) {
-      switch (context.classification) {
-      case 'box':
-        return 'py-2 pr-3 select-none'
-      default:
-        return 'font-medium block'
-      }
-    },
-    element: '',
-    input (context) {
-      switch (context.classification) {
-      case 'box':
-        return 'ml-4 mt-3'
-      default:
-        return ''
-      }
-    },
-    help: 'text-xs mb-1 text-gray-600',
-    error: 'text-red-700 text-xs mb-1'
-  }
+  locale: 'en'
 }
