135f8a7fc6ce02162aafb6835e89535b59978a99
alexandreargm <alexandreargm@gmail.com>
2021-07-16 16:10:45 +0200

    Added button sizes

--- components/_elements/Button.vue
+++ components/_elements/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="button" :class="getComponentVariant">
+  <button class="button" :class="getComponentVariant" @click="$emit('click')">
     <slot />
   </button>
 </template>
@@ -21,8 +21,10 @@ export default {
 
 <style lang="scss">
 .button {
+  flex-shrink: 0;
   min-height: 40px;
-  @apply rounded-full bg-b-light px-8 font-semibold select-none flex items-center whitespace-no-wrap;
+  min-width: 40px;
+  @apply rounded-full bg-b-light px-8 font-semibold select-none flex items-center whitespace-no-wrap text-brand;
 }
 
 .button:disabled {
@@ -39,21 +41,37 @@ export default {
 .button--primary {
   @apply bg-brand border-brand text-white;
 
-  &.button--simple {
+  &.button--simple,
+  &.button--outline {
     @apply text-brand bg-opacity-0;
   }
 }
 .buttton--secondary {
   @apply bg-b-dark border-b-dark text-t-dark-secondary;
+
+  &.button--simple,
+  &.button--outline {
+    @apply text-t-dark-secondary bg-opacity-0;
+  }
 }
 
 .button--sm {
   min-height: 32px;
+  min-width: 32px;
   @apply text-sm px-4;
+}
+.button--md {
+  min-height: 40px;
+  min-width: 40px;
+  @apply text-base px-4;
 }
 .button--lg {
   min-height: 64px;
+  min-width: 64px;
   @apply text-lg px-8;
+}
+.button--icon {
+  @apply p-0 justify-center;
 }
 
 .button--fluid {
